6d5508844ca315f930004c66f74a990d8f452e30
Vlad Marii <mariyvladyslav@gmail.com>
2024-02-29 02:17:34 +0200

    add modal and first page ready

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function toggleStar(checkbox) {
     checkbox.querySelector('input[type="checkbox"]').checked = !checkbox.querySelector('input[type="checkbox"]').checked;
 }
+function openModal() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal').style.display = 'none';
+}
